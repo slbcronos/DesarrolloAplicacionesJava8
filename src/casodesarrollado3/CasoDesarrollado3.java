1ef8bf612a50e0d4017ab46365bac3161efd58ac
Salvador LB <slb_18@hotmail.com>
2024-04-27 20:43:31 -0600

    Preparado

---
+++ src/casodesarrollado3/CasoDesarrollado3.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package casodesarrollado3;
+
+/**
+ *
+ * @author salvadorlb
+ */
+public class CasoDesarrollado3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Caso desarrollado #3 Pag 115
+        
+        // inicializar el form y hacerlo visible
+        frmPrincipal form = new frmPrincipal();
+        form.setVisible(true);
+        
+        
+    }
+    
+}
