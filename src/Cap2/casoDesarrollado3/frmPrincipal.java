ee584635e474e8c171817402b48ddf8e1977264e
Salvador LB <slb_18@hotmail.com>
2024-04-27 20:49:29 -0600

    cambio de carpetas en linux

--- src/Cap2/casoDesarrollado3/frmPrincipal.java
+++ src/Cap2/casoDesarrollado3/frmPrincipal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package casodesarrollado3;
+package Cap2.casoDesarrollado3;
 
 /**
  *
